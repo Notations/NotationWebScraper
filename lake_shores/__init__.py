9b6c2227daf8395168af23eadb005bf7f43363c3
sanXore <h.sefiane7@gmail.com>
2017-08-12 19:25:49 +0200

    Scafold projest + minimize .gitignore

---
+++ lake_shores/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+#title           :__init__.py
+#description     :This will create a header for a python script.
+#author          :Hamza Sefiane
+#date            :12/08/17
+#usage           :python __init__.py
+#notes           :
+#python_version  :3.4.3  
+#==============================================================================
